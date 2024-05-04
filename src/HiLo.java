496664f01da63d28e0d8cdc09f3a7272933db797
Bryce72 <brycedunlapsf56@gmail.com>
2024-05-04 15:25:09 -0400

    additonal playagain feature

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,39 +1,35 @@
 import java.util.Scanner;
 public class HiLo
 {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         // creates a random number for the user to guess
-        int theNumber = (int)(Math.random() * 100 + 1);
+        int theNumber = (int) (Math.random() * 100 + 1);
 
         int guess = 0;
+        String playAgain = "";
 
-        System.out.println("Pick a number 1-1000: ");
+        do {
+            System.out.println("Pick a number 1-100: ");
+            guess = scan.nextInt();
 
-        guess  = scan.nextInt();
+            System.out.println("You entered " + guess + ".");
 
-        System.out.println("You entered " + guess + ".");
+            while (guess != theNumber) {
+                if (guess < theNumber) {
+                    System.out.println("Your guess is too low. Pick another number: ");
+                    guess = scan.nextInt();
+                } else {
+                    System.out.println("Your guess is too high. Pick another number: ");
+                    guess = scan.nextInt();
+                }
+            }
 
-       while (guess != theNumber)
-       {
-           if(guess < theNumber)
-           {
-               System.out.println("Your guess is too low. Pick another number: ");
-               guess  = scan.nextInt();
-           }
-           else
-           {
-               System.out.println("Your guess is too high. Pick another number: ");
-               guess  = scan.nextInt();
-           }
-       }
+            System.out.println("Congrats you picked the right number! The correct number was " + theNumber + "!");
+            System.out.println("Would you like to play again? Type either Y for yes or N for no. ");
+            playAgain = scan.next();
 
-       System.out.println("Congrats you picked the right number! The correct number was " + theNumber + "!");
-
-
-
-
+        }while (playAgain.equalsIgnoreCase("y"));
     }
 }
