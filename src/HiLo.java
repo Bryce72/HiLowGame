b011a42ead93454f9bcbcd9731af8339b0292f54
Bryce72 <brycedunlapsf56@gmail.com>
2024-05-04 15:13:11 -0400

    Fully functioning game.. possibly too easy of a task

--- src/HiLo.java
+++ src/HiLo.java
@@ -1,10 +1,41 @@
+import java.util.Scanner;
 public class HiLo
 {
     public static void main(String[] args)
     {
+        Scanner scan = new Scanner(System.in);
+
         // creates a random number for the user to guess
         int theNumber = (int)(Math.random() * 100 + 1);
-        System.out.println(theNumber);
+
+        int guess = 0;
+
+        System.out.println("Pick a number 1-1000: ");
+
+        guess  = scan.nextInt();
+
+        System.out.println("You entered " + guess + ".");
+
+       while (guess != theNumber)
+       {
+           if(guess < theNumber)
+           {
+               System.out.println("Your guess is too low. Pick another number: ");
+               guess  = scan.nextInt();
+           }
+           else if(guess > theNumber)
+           {
+               System.out.println("Your guess is too high. Pick another number: ");
+               guess  = scan.nextInt();
+           }
+       }
+
+       if(guess == theNumber)
+       {
+           System.out.println("Congrats you picked the right number! The correct number was " + theNumber + "!");
+       }
+
+
 
     }
 }
