af8b3a3684640d8d0a6715133c869f5c068d7678
Bryce72 <brycedunlapsf56@gmail.com>
2024-05-04 14:50:56 -0400

    Basic Hi-Low game- added random number generator, 1-100

---
+++ src/HiLo.java
@@ -0,0 +1,10 @@
+public class HiLo
+{
+    public static void main(String[] args)
+    {
+        // creates a random number for the user to guess
+        int theNumber = (int)(Math.random() * 100 + 1);
+        System.out.println(theNumber);
+
+    }
+}
